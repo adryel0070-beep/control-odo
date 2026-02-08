d2d9ff40970cd8ce3b91d557f3cbafa67bada400
Jakt <adryel.0070@gmail.com>
2026-02-08 00:39:41 -0400

    Add page protection function using Supabase auth

---
+++ js/auth.js
@@ -0,0 +1,9 @@
+import { supabase } from './supabase.js'
+
+export async function protegerPagina() {
+  const { data } = await supabase.auth.getSession()
+
+  if (!data.session) {
+    window.location.href = 'index.html'
+  }
+}
